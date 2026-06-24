884f5cbd640ca0c0bd50fdbb3fecf23bc6bd5e48
DEEPAK GUPTA <deepakguptaa.3042@gmail.com>
2026-06-24 13:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (75.34%) - LeetSync

---
+++ 86-partition-list/partition-list.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+
+
+        if(head==NULL or head->next==NULL){return head;}
+
+        ListNode* small=new ListNode(0);
+        ListNode* h1=small;
+        ListNode* large=new ListNode(0);
+        ListNode* h2=large;
+        
+        ListNode* temp=head;
+        while(temp){
+            if(temp->val>=x){
+                large->next=temp;
+                large=large->next; 
+                temp=temp->next;
+                large->next=NULL;    
+            }
+            else{
+                small->next=temp;
+                small=small->next;
+                temp=temp->next;
+                small->next=NULL;
+            }
+            
+        }
+        small->next=h2->next;
+        return h1->next;
+    }
+};
