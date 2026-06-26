cfcdcec5033e771dfe0e43d2d65ad13ca710e1e0
DEEPAK GUPTA <deepakguptaa.3042@gmail.com>
2026-06-26 18:31:01 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (79.55%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -2,22 +2,47 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         // using prefix and suffix vector
+        // ismai extra memory lag gyi ab without memory karega
 
-        vector<int> prefix(nums.size(),0);
-        vector<int> suffix(nums.size(),0);
+        // vector<int> prefix(nums.size(),0);
+        // vector<int> suffix(nums.size(),0);
         
         
-        for(int i=1; i<nums.size(); i++){
-            prefix[i]=prefix[i-1]+ nums[i-1];
+        // for(int i=1; i<nums.size(); i++){
+        //     prefix[i]=prefix[i-1]+ nums[i-1];
+        // }
+        
+        // for(int i=nums.size()-2; i>=0; i--){
+        //     suffix[i]=suffix[i+1]+nums[i+1];
+        // }
+
+
+        // for(int i=0; i<prefix.size(); i++){
+        //     if(prefix[i]==suffix[i]){
+        //         return i;
+        //     }
+        // }
+
+        // return -1;
+
+        int sum=0;
+        for(int i=0; i<nums.size(); i++){
+            sum=sum+nums[i];
         }
-        
-        for(int i=nums.size()-2; i>=0; i--){
-            suffix[i]=suffix[i+1]+nums[i+1];
-        }
 
+        int temp=0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>0){
+                temp=nums[i-1]+temp;
+            }
+            else{
+                temp=0;
+            }
+            
 
-        for(int i=0; i<prefix.size(); i++){
-            if(prefix[i]==suffix[i]){
+            int x=sum-(temp+nums[i]);
+
+            if(temp==x){
                 return i;
             }
         }
