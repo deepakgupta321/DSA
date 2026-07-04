443044c748c6f5ed46b404c65ac8107c28a82f0b
DEEPAK GUPTA <deepakguptaa.3042@gmail.com>
2026-07-04 11:33:23 +0530

    Time: 59 ms (61.92%) | Memory: 134.1 MB (62.35%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+
+        vector<vector<pair<int,int>>> adj(n + 1);
+
+        for (auto &e : roads) {
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
+        }
+
+        vector<int> vis(n + 1, 0);
+        queue<int> q;
+
+        q.push(1);
+        vis[1] = 1;
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto &it : adj[node]) {
+                ans = min(ans, it.second);
+
+                if (!vis[it.first]) {
+                    vis[it.first] = 1;
+                    q.push(it.first);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
