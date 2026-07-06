6ea7864e14e9800a7d03a5190c93263b463907c8
DEEPAK GUPTA <deepakguptaa.3042@gmail.com>
2026-07-06 16:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (62.66%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,43 +1,63 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v;
-        v.push_back(-1);
+        // vector<int> v;
+        // ismai unordered map use karke easily kar skta hai'
+        // v.push_back(-1);
+        // stack<int> s;
+        // s.push(nums2[nums2.size()-1]);
+        // for(int i=nums2.size()-2; i>=0; i--){
+        //     if(s.top()>nums2[i]){
+        //         v.push_back(s.top());
+        //         s.push(nums2[i]);
+        //     }
+        //     else{
+        //         while(!s.empty()){
+        //             s.pop();
+        //             if(!s.empty() and s.top()>nums2[i]){
+        //                 v.push_back(s.top());
+        //                 break;
+        //             }
+        //         }
+        //         if(s.empty())
+        //            v.push_back(-1);
+        //         s.push(nums2[i]);
+        //     }
+        // }
+        // reverse(v.begin(), v.end());
+        // vector<int> ans;
+        // for(int i=0; i<nums1.size(); i++){
+        //     int x=nums1[i];
+        //     int j=0;
+        //     while(j<nums2.size()){
+        //         if(x==nums2[j]){
+        //             ans.push_back(v[j]);
+        //             j++;
+        //             break;
+        //         }
+        //         j++;
+        //     }
+        // }
+        // return ans;
+
         stack<int> s;
-        s.push(nums2[nums2.size()-1]);
-        for(int i=nums2.size()-2; i>=0; i--){
-            if(s.top()>nums2[i]){
-                v.push_back(s.top());
-                s.push(nums2[i]);
+        unordered_map <int,int> m;
+        for(int i=nums2.size()-1; i>=0; i--){
+            while(!s.empty() && s.top()<=nums2[i]){
+                s.pop();
             }
-            else{
-                while(!s.empty()){
-                    s.pop();
-                    if(!s.empty() and s.top()>nums2[i]){
-                        v.push_back(s.top());
-                        break;
-                    }
-                }
-                if(s.empty())
-                   v.push_back(-1);
-                s.push(nums2[i]);
+            if(s.empty()){
+                m[nums2[i]]=-1;
             }
+            else {
+                m[nums2[i]]=s.top();
+            }
+            s.push(nums2[i]);
         }
-        reverse(v.begin(), v.end());
-        vector<int> ans;
         for(int i=0; i<nums1.size(); i++){
-            int x=nums1[i];
-            int j=0;
-            while(j<nums2.size()){
-                if(x==nums2[j]){
-                    ans.push_back(v[j]);
-                    j++;
-                    break;
-                }
-                j++;
-            }
+            nums1[i]=m[nums1[i]];
         }
-        return ans;
+        return nums1;
 
     }
 };
