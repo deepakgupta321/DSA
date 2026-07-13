8a47d431b3e5d343fccd86717e2f737e7a93b601
DEEPAK GUPTA <deepakguptaa.3042@gmail.com>
2026-07-13 17:18:40 +0530

    Time: 7 ms (45.21%) | Memory: 113.7 MB (46.42%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,47 +2,44 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         deque<int> dq;
-        int result=0;
+        int result = 0;
+        int ff = 0;
+        int j = 0;
 
-        int ff=0;
-        int j=0;
-
-        while(j<nums.size()){
-
-            if(dq.size()==k){
-                if(dq.front()==1){
-                    ff=ff-dq.front();
+        while (j < nums.size()) {
+            
+            // Window maintenance (Yeh tumhara ekdum sahi hai!)
+            if (dq.size() == k) {
+                if (dq.front() == 1) {
+                    ff = ff - dq.front();
                 }
                 dq.pop_front();
-            
             }
 
-            if(nums[j]==0 && ff%2==0){
+            // Flip requirement check
+            if ((nums[j] == 0 && ff % 2 == 0) || (nums[j] == 1 && ff % 2 != 0)) {
+                
+                // 🚨 NAYA ADDITION: Kya flip karne ki jagah bachi hai?
+
+                if (j + k > nums.size()) {
+                    return -1; // Agar jagah nahi hai, toh possible hi nahi hai
+                }
+
                 ff++;
                 result++;
                 dq.push_back(1);
-                if(j+k>nums.size()){
-                    return -1;
-                }
-            }
-            else if(nums[j]==1 && ff%2!=0){
-                ff++;
-                result++;
-                dq.push_back(1);
-                if(j+k>nums.size()){
-                    return -1;
-                }
-            }
-            else{
+            } 
+
+            
+            else {
                 dq.push_back(0);
             }
+            
             j++;
         }
 
-        
+        // HATA DIYA: dq.back() == 1 wala logic. Uski zaroorat hi nahi ab.
 
         return result;
-
-        
     }
 };
