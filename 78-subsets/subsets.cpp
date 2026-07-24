807a1ee79ad7acfaa84233718a5541f65794c56f
DEEPAK GUPTA <deepakguptaa.3042@gmail.com>
2026-07-24 21:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (96.64%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    void sub(vector<vector<int>> &ans, vector<int> a, vector<int> nums, int i){
+    // void sub(vector<vector<int>> &ans, vector<int> a, vector<int> nums, int i){
 
-        if(i==nums.size()){
-            ans.push_back({a});
+    //     if(i==nums.size()){
+    //         ans.push_back({a});
+    //         return;
+    //     }
+
+    //     a.push_back(nums[i]);
+    //     sub(ans, a,nums,i+1 );
+
+    //     a.pop_back();
+    //     sub(ans, a,nums,i+1 );
+
+    // }
+
+    void sub(vector<int>& nums, vector<int> &temp, int i, vector<vector<int>>& ans){
+
+        if(i>nums.size()-1){
+            ans.push_back(temp);
             return;
         }
 
-        a.push_back(nums[i]);
-        sub(ans, a,nums,i+1 );
+        //include
+        temp.push_back(nums[i]);
+        sub(nums, temp, i+1, ans);
 
-        a.pop_back();
-        sub(ans, a,nums,i+1 );
+        //backtrack woh element nhi lena tha////////
 
+        temp.pop_back();
+        sub(nums, temp, i+1, ans );
     }
 
 
@@ -20,11 +37,19 @@ public:
 
 
     vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>> ans;
+        // vector<int> a;
+        // int i=0;
+        // sub(ans,a,nums,i);
+        // return ans;
+
         vector<vector<int>> ans;
-        vector<int> a;
-        int i=0;
-        sub(ans,a,nums,i);
+        vector<int> temp;
+        int index=0;
+        sub(nums, temp, index, ans );
+
         return ans;
+
 
     }
 };
